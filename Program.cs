ecbae6e31aea0fbe3079a2bbcbf7e8f1473a3118
israel lopes <israel.lopes2710@gmail.com>
2021-03-05 12:37:21 -0300

    aranha

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AsciiArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Beep();
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Etec Adolpho Berezin");
+            Console.WriteLine("Feito por Israel Camilo Lopes");
+            Console.WriteLine("1I3");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine();
+            Console.WriteLine("  ||  || ");
+            Console.WriteLine("  \\\\()// ");
+            Console.WriteLine(" //(__)\\\\ ");
+            Console.WriteLine(" ||    ||");
+            Console.WriteLine();
+            Console.WriteLine("Consegui! kkkkk, eu tava com muita dificuldade mas de tanto tentar uma hora deu certo");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Clique em uma tecla para limpar a tela");
+            Console.ReadKey();
+            Console.ResetColor();
+            Console.Clear();
+        }
+    }
+}
