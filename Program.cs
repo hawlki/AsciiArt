24d4fafb2d5af752053289102dbba0a02cec596a
israel lopes <israel.lopes2710@gmail.com>
2021-03-21 16:12:52 -0300

    organiza arquivo

--- Program.cs
+++ Program.cs
@@ -8,20 +8,24 @@ namespace AsciiArt
         {
             Console.Clear();
             Console.Beep();
+
             Console.WriteLine("-----------------------------");
             Console.WriteLine("Etec Adolpho Berezin");
             Console.WriteLine("Feito por Israel Camilo Lopes");
             Console.WriteLine("1I3");
             Console.WriteLine("-----------------------------");
             Console.WriteLine();
+
             Console.WriteLine("  ||  || ");
             Console.WriteLine("  \\\\()// ");
             Console.WriteLine(" //(__)\\\\ ");
             Console.WriteLine(" ||    ||");
             Console.WriteLine();
+
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Clique em uma tecla para limpar a tela");
             Console.ReadKey();
+            
             Console.ResetColor();
             Console.Clear();
         }
