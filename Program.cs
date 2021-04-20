111121a32068faa4793f4e2ceee9d811f5c7a42f
israel lopes <israel.lopes2710@gmail.com>
2021-04-20 10:10:24 -0300

    adicona nome do projeto

--- Program.cs
+++ Program.cs
@@ -9,11 +9,9 @@ namespace AsciiArt
             Console.Clear();
             Console.Beep();
 
-            Console.WriteLine("-----------------------------");
-            Console.WriteLine("Etec Adolpho Berezin");
-            Console.WriteLine("Feito por Israel Camilo Lopes");
-            Console.WriteLine("1I3");
-            Console.WriteLine("-----------------------------");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("          Asciiart          ");
+            Console.WriteLine("----------------------------");
             Console.WriteLine();
 
             Console.WriteLine("  ||  || ");
